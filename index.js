d8169559896c14d97a20df014844e1442506de96
merellois <merellois@github.com>
2021-01-13 19:15:53 +0100

    Version 1

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const SCROLLSLIDE = document.querySelector('.scroll-slider__slide');
+
+// scrolloffset should be the width of the slide
+const SCROLLOFFSET = SCROLLSLIDE.offsetWidth;
+
+const SCROLLSLIDERWRAPPER = document.querySelector('.scroll-slider__wrapper');
+
+const NAVIGATENEXT = document.querySelector('.scroll-slider__navigation-item--next');
+const NAVIGATEPREV = document.querySelector('.scroll-slider__navigation-item--prev');
