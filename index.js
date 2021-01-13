3c2b5366b18f57e0f61cded6ef265a263c1bbb56
merellois <merellois@github.com>
2021-01-14 00:41:13 +0100

    Version 3

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ const SCROLLSLIDERWRAPPER = document.querySelector('.scroll-slider__wrapper');
 
 const NAVIGATENEXT = document.querySelector('.scroll-slider__navigation-item--next');
 const NAVIGATEPREV = document.querySelector('.scroll-slider__navigation-item--prev');
+
+window.addEventListener("scroll", function() {showFunction()});
+
+function showFunction() {
+    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+        document.getElementById("toptexts2").style.display = "block";
+    } else {
+        document.getElementById("toptexts2").style.display = "none";
+    }
+}
