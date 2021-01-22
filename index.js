cdc6772cd3d3a7d6116cfd67c95d4bc40ae1ce2d
merellois <merellois@github.com>
2021-01-22 17:07:13 +0100

    2021_22_01: 17:07

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
+
 const SCROLLSLIDE = document.querySelector('.scroll-slider__slide');
 
 // scrolloffset should be the width of the slide
@@ -8,3 +19,12 @@ const SCROLLSLIDERWRAPPER = document.querySelector('.scroll-slider__wrapper');
 
 const NAVIGATENEXT = document.querySelector('.scroll-slider__navigation-item--next');
 const NAVIGATEPREV = document.querySelector('.scroll-slider__navigation-item--prev');
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
